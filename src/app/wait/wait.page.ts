0cf9533848234f574671dd65d6a5754bf644bd86
Pornnapat Jan <pornnapat.jan@trrgroup.com>
2019-07-15 15:58:19 +0700

    wait and finish jaaaa

---
+++ src/app/wait/wait.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-wait',
+  templateUrl: './wait.page.html',
+  styleUrls: ['./wait.page.scss'],
+})
+export class WaitPage implements OnInit {
+
+  constructor(private nav: NavController) {
+    
+   }
+
+  ngOnInit() {
+  }
+  waitClick(){
+    this.nav.navigateForward("/detailbike");
+  }
+}
